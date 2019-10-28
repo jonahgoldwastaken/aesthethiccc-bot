183a453c0de0f191956af332430aea884e28e466
Jonah Meijers <jonahmeijers97@gmail.com>
2019-10-28 10:18:27 +0100

    reducer initialvalue put in proper place

--- aestheticfy.js
+++ aestheticfy.js
@@ -1,9 +1,9 @@
 const aesthetic = text => {
   const textArray = text.split("");
-  return textArray.reduce((acc = "", curr, i) => {
+  return textArray.reduce((acc, curr, i) => {
     if (i === textArray.length - 1) return (acc += curr.toUpperCase());
     return (acc += `${curr.toUpperCase()} `);
-  });
+  }, "");
 };
 
 module.exports = aesthetic;
